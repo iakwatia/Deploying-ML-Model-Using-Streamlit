e7eec9fcba283f516f0b4d03fd806f3472b7bcb5
Isaac Asante-Kwatia <isaac.asantekwatia@gmail.com>
2024-06-03 15:44:48 +0000

    update app

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 
 # Set page configuration
-st.set_page_config(page_title="Customer Churn Prediction", layout="wide")
+#st.set_page_config(page_title="Customer Churn Prediction", layout="wide")
 
 st.title("Customer Churn Prediction")
+
