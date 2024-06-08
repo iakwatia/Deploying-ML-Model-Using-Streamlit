6c8b8b73b6242340ff0b85a1cbf9472f1041be2a
Isaac Asante-Kwatia <isaac.asantekwatia@gmail.com>
2024-06-08 11:07:15 +0000

    update predict page

--- Pages/3_Predict.py
+++ Pages/3_Predict.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import joblib
 
 st.set_page_config(
     page_title='Predict Page',
@@ -6,3 +8,37 @@ st.set_page_config(
 )
 
 st.title('Make Customer Churn Predictions')
+
+
+st.cache_resource()
+def load_forest_pipeline():
+    pipeline = joblib.load('./model/forest_pipeline.joblib')
+    return pipeline
+
+
+st.cache_resource()
+def load_logistic_pipeline():
+    pipeline = joblib.load('./model/logistic_pipeline.joblib')
+    return pipeline
+
+
+st.cache_resource(show_spinner='Models loading...')
+def select_model():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.selectbox('select a model', options=['Random Forest', 'Logistic Regression'], key='selected_model')
+    with col2:
+        pass
+
+    if st.session_state['selected_model'] == 'Random Forest':
+        pipeline = load_forest_pipeline()
+    else:
+        pipeline = load_logistic_pipeline()
+
+    encoder = joblib.load('./model/encoder.joblib')
+
+    return pipeline, encoder
+
+
+
